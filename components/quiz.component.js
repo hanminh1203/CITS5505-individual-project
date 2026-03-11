63b480ae0cdaf66f7a7fc8bfe72b52330bad5f71
Mark Tran <tranhanminh34@gmail.com>
2026-03-11 16:28:57 +0800

    Correct variable declaration to avoid duplication when moving forth and back

--- components/quiz.component.js
+++ components/quiz.component.js
@@ -1,6 +1,8 @@
-const PASSING_SCORE = 5;
-const NUMBER_OF_QUESTIONS = 10;
+
 $(function () {
+    const PASSING_SCORE = 5;
+    const NUMBER_OF_QUESTIONS = 10;
+
     const questionTemplate = (question, questionIndex) => `
     <div id="question-${questionIndex}" class="question">
         <p>${question.question}</p>
@@ -52,44 +54,43 @@ $(function () {
         localStorage.removeItem('results');
         $('#results-list').html('');
     });
-});
-
-function loadQuestions(selectedQuestions, questionTemplate) {
-    $('#quiz-container').html(selectedQuestions.map(questionTemplate).join(''));
-}
-
-function calculateScore(selectedQuestions, selectedAnswers) {
-    let score = 0;
-    for (let i = 0; i < selectedQuestions.length; i++) {
-        const correctAnswer = selectedQuestions[i].answer;
-        const selectedAnswer = selectedAnswers[i];
-        const feedbackElement = $(`#question-${i} .feedback`);
-        if (selectedAnswer === correctAnswer) {
-            score++;
-            // TODO: clear class instead of removeClass
-            feedbackElement.removeClass('text-danger').text('Correct!').addClass('text-success');
-        } else {
-            feedbackElement.removeClass('text-success').text(`Incorrect! The correct answer is: ${selectedQuestions[i].choices[correctAnswer]}`).addClass('text-danger');
-        }
+    function loadQuestions(selectedQuestions, questionTemplate) {
+        $('#quiz-container').html(selectedQuestions.map(questionTemplate).join(''));
     }
-    return score;
-}
-
-function randomizeQuestions(questions) {
-    return questions.sort(() => 0.5 - Math.random()).slice(0, NUMBER_OF_QUESTIONS);
-}
-
-function extractSelectedAnswers() {
-    return $('#quiz-container').serializeArray()
-        .reduce((values, x) => {
-            const [_, questionIndex] = x.name.match(/question-(\d+)/);
-            values[questionIndex] = parseInt(x.value);
-            return values;
-        }, {})
-}
-
-function resetQuiz() {
-    $('#quiz-container').html('');
-    $('#btn-submit').removeClass('invisible');
-    $('#btn-reset').addClass('invisible');
-}
+    
+    function calculateScore(selectedQuestions, selectedAnswers) {
+        let score = 0;
+        for (let i = 0; i < selectedQuestions.length; i++) {
+            const correctAnswer = selectedQuestions[i].answer;
+            const selectedAnswer = selectedAnswers[i];
+            const feedbackElement = $(`#question-${i} .feedback`);
+            if (selectedAnswer === correctAnswer) {
+                score++;
+                // TODO: clear class instead of removeClass
+                feedbackElement.removeClass('text-danger').text('Correct!').addClass('text-success');
+            } else {
+                feedbackElement.removeClass('text-success').text(`Incorrect! The correct answer is: ${selectedQuestions[i].choices[correctAnswer]}`).addClass('text-danger');
+            }
+        }
+        return score;
+    }
+    
+    function randomizeQuestions(questions) {
+        return questions.sort(() => 0.5 - Math.random()).slice(0, NUMBER_OF_QUESTIONS);
+    }
+    
+    function extractSelectedAnswers() {
+        return $('#quiz-container').serializeArray()
+            .reduce((values, x) => {
+                const [_, questionIndex] = x.name.match(/question-(\d+)/);
+                values[questionIndex] = parseInt(x.value);
+                return values;
+            }, {})
+    }
+    
+    function resetQuiz() {
+        $('#quiz-container').html('');
+        $('#btn-submit').removeClass('invisible');
+        $('#btn-reset').addClass('invisible');
+    }
+});
