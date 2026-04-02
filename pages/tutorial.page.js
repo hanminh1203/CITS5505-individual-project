0a3834637d3bdddae31a71ff1425afae86d4326b
Mark Tran <tranhanminh34@gmail.com>
2026-04-02 10:26:15 +0800

    Fix some bugs: - Add tabs - Fix overflow on pre - Fix z-index of flipcard

--- pages/tutorial.page.js
+++ pages/tutorial.page.js
@@ -25,10 +25,23 @@ class TutorialComponent {
         });
 
         $('.box-model').on('mouseover', (event) => {
-            $(event.target).removeClass('box-white');
-            $(`.box-model:not(#${event.target.id})`).addClass('box-white');
-        }).on('mouseout', (event) => {
+            const hoveredBox = event.currentTarget;
+            $('.box-model').each((_, element) => {
+                $(element).toggleClass('box-white', element !== hoveredBox);
+            });
+        }).on('mouseout', () => {
             $('.box-model').removeClass('box-white');
+        });
+
+        $("button[app-module-tab-name]").on('click', (event) => {
+            const moduleName = $(event.currentTarget).attr('app-module-tab-name');
+            $(`button[app-module-tab-name="${moduleName}"]`).addClass('active').siblings('button').removeClass('active');
+            $(`.lesson[app-module-name="${moduleName}"]`).addClass('active').siblings('.lesson').removeClass('active');
+            const pageHeader = $('.page-header');
+            $("#scrolling-div")[0].scrollTo({
+                top: pageHeader[0].offsetTop + pageHeader[0].offsetHeight,
+                behavior: 'smooth'
+            });
         });
     }
 }
